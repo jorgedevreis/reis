81cbebffc0c18f0745e8be526530cdeaf6c8aa6f
jorgeesite <contatojorgee@gmail.com>
2024-11-08 16:39:11 -0300

    atualizacao do portfolio

---
+++ app.js
@@ -0,0 +1,77 @@
+class FormSubmit {
+  constructor(settings) {
+    this.settings = settings;
+    this.form = document.querySelector(settings.form);
+    this.formButton = document.querySelector(settings.button);
+    if (this.form) {
+      this.url = this.form.getAttribute("action");
+    }
+    this.sendForm = this.sendForm.bind(this);
+  }
+
+  displaySuccess() {
+    this.form.innerHTML = this.settings.success;
+  }
+
+  displayError() {
+    this.form.innerHTML = this.settings.error;
+  }
+
+  getFormObject() {
+    const formObject = {};
+    const fields = this.form.querySelectorAll("[name]");
+    fields.forEach((field) => {
+      formObject[field.getAttribute("name")] = field.value;
+    });
+    return formObject;
+  }
+
+  onSubmission() {
+    this.formButton.disabled = true;
+    this.formButton.innerText = "Enviando...";
+  }
+
+  async sendForm(event) {
+    event.preventDefault();
+    if (!this.url) {
+      console.error("URL do formulário não definida.");
+      this.displayError();
+      return;
+    }
+
+    this.onSubmission();
+
+    try {
+      const response = await fetch(this.url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(this.getFormObject()),
+      });
+
+      if (response.ok) {
+        this.displaySuccess();
+      } else {
+        this.displayError();
+      }
+    } catch (error) {
+      this.displayError();
+      console.error("Erro ao enviar o formulário:", error);
+    }
+  }
+
+  init() {
+    if (this.form) this.formButton.addEventListener("click", this.sendForm);
+    return this;
+  }
+}
+
+const formSubmit = new FormSubmit({
+  form: "[data-form]",
+  button: "[data-button]",
+  success: "<h1 class='success'>Mensagem enviada!</h1>",
+  error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
+});
+formSubmit.init();
